04e1bd90926f8727142fcc4e6c0e8318a817c0a2
Tobias Fechner <1fechner@informatik.uni-hamburg.de>
2015-05-12 20:39:15 +0200

    awrouheg

--- Aufgabe5/src/de/dis2015/jtcdbs/PersistenceManagerImpl.java
+++ Aufgabe5/src/de/dis2015/jtcdbs/PersistenceManagerImpl.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 
 public class PersistenceManagerImpl implements PersistenceManager {
 
-	private static final String _persistancePath = "persistentDataStorage/";
+	private static final String _persistanceStoragePath = "persistentDataStorage/";
 	private static final String _fileExtension = ".ppg"; // ppg ~ PersistentPaGe
-	private static final String _fileSeparator = ".";
+	private static final String _separator = ";";
+
+	private long _lastLSN = 0;
 
 	@Override
 	public int beginTransaction() {
@@ -23,15 +25,17 @@ public class PersistenceManagerImpl implements PersistenceManager {
 
 	@Override
 	public void write(int tx, int pageId, String data) {
-		// TODO Hier fehlt bestimmt noch was
-		writePage(tx, pageId, data);
+		// TODO Hier noch die LSN
+		writePage(pageId, -1, data);
+		writeLogEntry(tx, pageId, data);
 	}
 
-	private boolean writePage(int tx, int pageId, String data) {
+	private boolean writePage(int pageId, int lsn, String data) {
 		try {
-			FileWriter fw = new FileWriter(_persistancePath + _fileSeparator
-					+ tx + _fileSeparator + pageId + _fileExtension);
-			fw.write(tx + pageId + data);
+			FileWriter fw = new FileWriter(_persistanceStoragePath + pageId
+					+ _fileExtension);
+
+			fw.write(pageId + _separator + lsn + _separator + data);
 			fw.flush();
 			fw.close();
 		} catch (IOException e) {
@@ -41,5 +45,28 @@ public class PersistenceManagerImpl implements PersistenceManager {
 
 		// Page successfully written
 		return true;
+	}
+
+	private boolean writeLogEntry(int tx, int pageId, String data) {
+		try {
+			FileWriter fw = new FileWriter(_persistanceStoragePath + tx + "."
+					+ pageId + _fileExtension);
+
+			fw.write(tx + _separator + pageId + _separator + getNextLSN()
+					+ _separator + data);
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		// Log successfully written
+		return true;
+	}
+
+	private long getNextLSN() {
+		_lastLSN += 1;
+		return _lastLSN;
 	}
 }
